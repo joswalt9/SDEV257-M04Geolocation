5d02bab23cc6a274a204cffac493955c19e218a9
joswalt9 <joswalt9@ivytech.edu>
2024-11-13 23:43:33 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StatusBar } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import styles from "./styles";
 
-export default function App() {
+StatusBar.setBarStyle("dark-content");
+
+export default () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      // Request permission for location access
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        setLocation({ latitude: 37.7749, longitude: -122.4194 });
+        return;
+      }
+
+      // Get the user's current location
+      let locationData = await Location.getCurrentPositionAsync({});
+      setLocation(locationData.coords);
+    };
+
+    getLocation();
+  }, []);
+
+  //Restaurant Location
+  const restaurant = {
+    latitude: 39.63145,
+    longitude: -86.11475,
+    title: "Dominos",
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location ? (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.mapView}
+          showsUserLocation
+          followUserLocation
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: restaurant.latitude,
+              longitude: restaurant.longitude,
+            }}
+            title={restaurant.title}
+            description={restaurant.description}
+          />
+        </MapView>
+      ) : (
+        <Text>Loading location...</Text>
+      )}
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
